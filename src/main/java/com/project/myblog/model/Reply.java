0ccc8ebb366f9f18defb091069af016ed7cb38fd
hongxeob <sjun2918@naver.com>
2023-01-14 16:06:15 +0900

    feat: Reply(댓글) 테이블 생성

---
+++ src/main/java/com/project/myblog/model/Reply.java
@@ -0,0 +1,37 @@
+package com.project.myblog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor //파라미터가 없는 기본 생성자를 생성
+@AllArgsConstructor //모든 필드 값을 파라미터로 받는 생성자를 만듦
+@Builder
+@Entity
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 400)
+    private String content;
+
+    @ManyToOne // 연관관계 형성
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    @ManyToOne // 연관관계 형
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @DateTimeFormat(pattern = "yyyy-mm-dd HH:mm")
+    private LocalDateTime localDateTime;
+
+
+}
