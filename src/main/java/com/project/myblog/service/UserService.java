f62f0b9c13a6c666f4fd679fbdf46f8eb100457d
hongxeob <sjun2918@naver.com>
2023-01-19 12:17:26 +0900

    feat: 로그인시 비밀번화 해쉬화

--- src/main/java/com/project/myblog/service/UserService.java
+++ src/main/java/com/project/myblog/service/UserService.java
@@ -4,6 +4,7 @@ import com.project.myblog.model.User;
 import com.project.myblog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
 
     @Transactional
     public void join(User user) {
+        String originPassword = user.getPassword();
+        String encodePassword = encoder.encode(originPassword);
+        user.setPassword(encodePassword);
         userRepository.save(user);
     }
 
