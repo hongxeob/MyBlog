5ad276a7c2b80c51b557930507d820656f797c5b
hongxeob <sjun2918@naver.com>
2023-01-19 12:19:41 +0900

    feat: 스프링 시큐리티 로그인 사용 - loginForm(form의 action 사용)

--- src/main/java/com/project/myblog/config/SecurityConfig.java
+++ src/main/java/com/project/myblog/config/SecurityConfig.java
@@ -6,24 +6,32 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@Configuration //빈으로 등록
 @EnableWebSecurity // 시큐리티 필터 추가 = Spring security 활성화. 설정을 해당 파일에서 한다
 @EnableGlobalMethodSecurity(prePostEnabled = true)//스프링 시큐리티 활성화 / 해당 메서드 실행 전 체크!
 public class SecurityConfig {
     @Bean
+    //비밀번호 해쉬화
+    public BCryptPasswordEncoder encoderPW() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/auth/loginForm");
+                .csrf().disable() // CSRF 보안에 대한 설정. 아무 설정도 하지 않으면 CSRF 보안을 하도록 설정
+                .authorizeHttpRequests() //요청에 대한 권한 지정. Security 처리에 HttpServletRequest를 이용한다는 것을 의미한다.
+                .antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**").permitAll()//특정 경로를 지정->어떤 사용자든 접근 가능
+                .anyRequest() //설정한 경로 외에 모든 경로를 말함
+                .authenticated() // 인증된 사용자만이 접근 가능
+            .and()
+                .formLogin() // form 기반의 로그인을 사용
+                .loginPage("/auth/loginForm") // 로그인 페이지 url 설정
+                .loginProcessingUrl("/auth/loginProc") // 로그인 Form Action Url을 지정할 수 있다. 사용자 이름과 암호를 제출할 URL (기본값은 /login 이다.)
+                .defaultSuccessUrl("/"); //로그인에 성공했을 때에 아무런 설정을 하지 않았을 시 넘어가는 페이지를 설정
 
         return http.build();
     }
---
+++ src/main/java/com/project/myblog/config/auth/PrincipalDetail.java
@@ -0,0 +1,60 @@
+package com.project.myblog.config.auth;
+
+import com.project.myblog.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+// 스프링 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고, 완료가 되면 UserDetails 타입의 오브젝트를 스프링 시큐리티의 고유한 세션저장소에 저장을 해준다.
+public class PrincipalDetail implements UserDetails { //UserDetails = Spring Security 에서 사용자의 정보를 담는 인터페이스
+
+    private final User user;
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    //계정 만료 여부 (true:만료X)
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    //계정 잠김 여부 (true:잠김X)
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    //비밀번호 만료 여부 (true:만료X)
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    //계정이 사용 가능한지 (true:활성화(사용가능)
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    //계정의 권한 목록 리턴
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(() -> {
+            return "ROLE_" + user.getRole();
+        });
+        return collection;
+    }
+}
