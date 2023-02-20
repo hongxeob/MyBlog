fb0b8b0feb2d539a28dc390cf26b9da89df5db59
hongxeob <sjun2918@naver.com>
2023-02-21 00:20:34 +0900

    feat: 구글,페이스북 로그인 구현4

--- src/main/java/com/project/myblog/config/auth/PrincipalDetails.java
+++ src/main/java/com/project/myblog/config/auth/PrincipalDetails.java
@@ -2,20 +2,52 @@ package com.project.myblog.config.auth;
 
 import com.project.myblog.model.User;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
-@RequiredArgsConstructor
 @Getter
 // 스프링 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고, 완료가 되면 UserDetails 타입의 오브젝트를 스프링 시큐리티의 고유한 세션저장소에 저장을 해준다.
-public class PrincipalDetail implements UserDetails { //UserDetails = Spring Security 에서 사용자의 정보를 담는 인터페이스
+public class PrincipalDetails implements UserDetails, OAuth2User { //UserDetails = Spring Security 에서 사용자의 정보를 담는 인터페이스
 
-    private final User user;
+    private User user;
+    public Map<String, Object> attributes;
 
+
+    //UserDetails : Form 로그인 시 사용
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    //OAuth2User : OAuth2 로그인 시 사용
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        //PrincipalOauth2UserService 참고
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    //계정의 권한 목록 리턴
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(() -> {
+            return "ROLE_" + user.getRole();
+        });
+        return collection;
+    }
 
     @Override
     public String getPassword() {
@@ -51,13 +83,9 @@ public class PrincipalDetail implements UserDetails { //UserDetails = Spring Sec
         return true;
     }
 
+
     @Override
-    //계정의 권한 목록 리턴
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> collection = new ArrayList<>();
-        collection.add(() -> {
-            return "ROLE_" + user.getRole();
-        });
-        return collection;
+    public String getName() {
+        return null;
     }
 }
